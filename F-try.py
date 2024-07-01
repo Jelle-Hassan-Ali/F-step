8c46987b20b6285ee4c12ce97bd9a0fe43f041c7
Jelle H Ali <jellehassanali60@gmail.com>
2024-07-01 11:00:17 +0300

    Firt step off

---
+++ F-try.py
@@ -0,0 +1,53 @@
+
+# warmup
+
+print("Hello World!")
+print("Hello Jelle Welcome!")
+
+# Variables
+name = ['Jelle','Hassan']
+print(name)
+
+name = 'Jelle Hassan Ali'
+print(name)
+
+P_name = 'John'
+P_age = 30
+is_anewPatient = True
+print(P_name)
+print(P_age)
+print(is_anewPatient)
+list_of_names = ['Jelle', 'Hassan', 'Ali']
+
+# input Functions
+Name =input('Magacaa? ')
+print('Hye '+ Name + ' Welcome')
+
+Age =input('What is Your age ? ')
+print('Hello Your age is '+ Age )
+
+S_Name =input('S_Name ? ')
+print('hey '+ S_Name + ' Welcome to Our college!')
+
+# input functions
+
+P_Name =input('what is your name ? ')
+Color =input('what is your favourite color ? ')
+print(' Hi🖐  '  +  P_Name + ' You like the ' + Color + ' color')
+
+# Type Conversion
+
+Birth_Year =input('Birth Year ? ')
+
+Age = 2024 - int(Birth_Year)
+print(Age )
+
+weight_lbs = input('Enter Your Weight (lbs): ')
+
+Weight_kg = float(weight_lbs) * 0.453592
+print(Weight_kg)
+
+weight_kg = input('Enter Your Weight (kg): ')
+
+Weight_kg = float(weight_lbs) * 2.2
+print(weight_lbs)
