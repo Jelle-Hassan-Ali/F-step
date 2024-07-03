22b5c1088f3a68e03b8d13f0df246236f0c6584a
Jelle H Ali <jellehassanali60@gmail.com>
2024-07-03 11:30:39 +0300

    Day of tree

---
+++ Day-three.py
@@ -0,0 +1,86 @@
+# Arithmatic Operations
+
+""" Additional """
+num1 = 18
+num2 = 2
+
+sum = num1 + num2
+print (sum)
+
+a = 47
+b = 53
+add = a + b
+print (add)
+
+""" subtractions"""
+x = 10
+y = 1
+sub = x - y
+print(sub)
+
+x1 = 10
+y1 = 4
+deff = x1 - y1
+print (deff)
+
+""" multiplication """
+x2 = 10
+y2 = 4
+z = x2 * y2
+print(z)
+
+""" Power """
+x3 = 10
+y3 = 3
+z1 = x3 ** y3
+print(z1)
+
+"""devision with remaining"""
+M = 20
+N = 3
+O = M / N
+print(O)
+
+"""devision without remaining"""
+
+M = 20
+N = 3
+O = M // N
+print(O)
+
+""" Modulus """
+
+print(20 % 4)
+
+
+""" Operator Predence """
+
+x = 10 + 3 * 2
+print(x)
+
+x = 10 + 3 * 2 ** 2
+print(x)
+
+x =  (3 + 2 ) * 10 - 3
+print(x)
+
+
+""" Math Functions """
+x = 3.9
+print(round(x))
+
+
+x = 3.9
+print(abs(-3.9))
+
+
+import math 
+print(math.ceil(7.4))
+
+import math 
+print(math.floor(7.8))
+
+import math 
+print(math.factorial(7))
+
+
