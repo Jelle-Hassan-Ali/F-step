8fa830d92197d05bed8801c2e2e52df774411d4a
Jelle H Ali <jellehassanali60@gmail.com>
2024-07-04 11:46:34 +0300

    Day of Four

---
+++ Day-four.py
@@ -0,0 +1,44 @@
+# if stament
+
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print('It is hot today')
+    print('Drink plenty water')
+
+elif is_cold:
+    print("It is cold today") 
+    print("make sure to wear warm clothes ") 
+    
+else:
+    print("It is a lovely day ") 
+    print("Enjoy Your Day")   
+    
+
+good_credit = False
+
+bad_credit = False
+
+if good_credit:
+    print("the buyer most but down $100,0000") 
+    
+elif bad_credit:
+    print("the buyer most but down $200,0000")     
+  
+else:
+    print("ka tag mesha qofyahoy shiidka ah")
+    
+    
+price = 1000000
+
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+    
+else:
+    down_payment = 0.2 * price
+        
+print(f"down_payment ${down_payment}")    
+       
