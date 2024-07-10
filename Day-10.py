174add0be3cba9e4fc2946f1f90d912390086ca7
Jelle H Ali <jellehassanali60@gmail.com>
2024-07-10 13:20:24 +0300

    Today

---
+++ Day-10.py
@@ -0,0 +1,32 @@
+command = ""
+
+started = False
+while True:
+    
+    command = input("> ").lower()
+    
+    if command == "start":
+        if started:
+            print("Car is already started.")
+        else:
+            started = True
+            print("Car started.")    
+        
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped ...")   
+        else:
+            started = False
+            print("Car stopped ...")
+            
+    elif command == "help":
+        print("""
+        start - Start the car
+        stop - Stop the car
+        quit - Exit the program
+        """)
+    elif command == "quit":
+        print("Goodbye!")
+        break
+    else:
+        print("I don't understand what you want.")  
